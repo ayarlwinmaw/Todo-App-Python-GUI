87973b0028ee8f78abc2d9d111affad0535a5d3d
Ayar Maw <ayarlwinmaw@gmail.com>
2025-10-13 01:16:37 -0700

    integrate write_todo function to Add button

--- main.py
+++ main.py
@@ -2,9 +2,24 @@ import functions
 import FreeSimpleGUI as fsg
 
 label = fsg.Text("Type in a todo")
-input_box = fsg.InputText(tooltip="Enter a todo")
+input_box = fsg.InputText(tooltip="Enter a todo", key="todo")
 add_button = fsg.Button("Add")
 
-window = fsg.Window('My Todo App', layout=[[label], [input_box, add_button]])
-window.read()
+window = fsg.Window('My Todo App',
+                    layout=[[label], [input_box, add_button]],
+                    font=('Helvetica', 12))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case fsg.WINDOW_CLOSED:
+            break
+
 window.close()
